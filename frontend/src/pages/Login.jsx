5f7adc543931497ebf48e8c0178ceae43d9d1288
DidasMba <daniel.mbarushi@gmail.com>
2024-02-22 20:30:41 +0300

    form edit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MyComponent.css';
 
-
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -14,49 +13,52 @@ function Login() {
 
   return (
     <div className="container mt-5">
-      <h1 className="text-center mb-4">Login</h1>
-      <form>
-        <div className="form-group col-md-6">
-          <label htmlFor="email">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+      <div className="d-flex justify-content-center"> {/* Centrage du formulaire */}
+        <div className="col-md-6">
+          <h1 className="text-center mb-4">Login</h1>
+          <form className="custom-form"> {/* Ajout de la classe personnalisée pour le formulaire */}
+            <div className="form-group">
+              <label htmlFor="email">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="form-group"> {/* Ajout d'une grande espace entre les deux formulaires */}
+            <div className="form-group d-flex justify-content-center mt-5"> {/* Utilisez d-flex et justify-content-center pour centrer le bouton */}
+              <button
+                type="button"
+                className="btn btn-success btn-block"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </div>
+            </div>
+          </form>
+          <div className="text-center mt-3">
+            <span>Don't have an account? </span>
+            <Link to="/signup">Sign Up</Link>
+          </div>
         </div>
-        <div className="form-group col-md-6">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="form-group d-flex justify-content-center mt-5"> {/* Utilisez d-flex et justify-content-center pour centrer le bouton */}
-        <button
-          type="button"
-        
-          className="btn btn-success btn-block" 
-          onClick={handleLogin}
-        >
-          Login
-        </button>
-        </div>
-      </form>
-      <div className="text-center mt-3">
-        <span>Don't have an account? </span>
-        <Link to="/signup">Sign Up</Link>
       </div>
     </div>
   );
 }
 
 export default Login;
-  
-
